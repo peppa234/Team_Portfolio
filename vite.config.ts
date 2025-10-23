544273020449007591fcfc8e2d3d4794dd2c6a9c
peppa234 <aichabrihmouche@gmail.com>
2025-10-23 10:40:55 +0100

    fix: fixed paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: '/Team_Portfolio/',
   css: {
     postcss: {
       plugins: [tailwind()],
